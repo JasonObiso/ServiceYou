4a7ec3258bf76a854d330d9f9545fdd4aa1d0068
John Marc Balbada <kumuro123@gmail.com>
2023-10-10 15:03:31 +0800

    [Marc] - Adding apps in INSTALLED_APPS

--- ServiceYou/ServiceYou/settings.py
+++ ServiceYou/ServiceYou/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Account',
+    'AcceptRequest',
+    'PayRequest',
+    'PostService',
+    'RateService',
+    'RequestService',
 ]
 
 MIDDLEWARE = [
