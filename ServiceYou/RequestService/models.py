27eed2a27fa54868eee104e9e4c82a162068d405
kuian2202 <bernaldezkurtian@gmail.com>
2023-10-12 12:42:53 +0800

    venv

--- ServiceYou/RequestService/models.py
+++ ServiceYou/RequestService/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=10, primary_key=True, null=False)
+    password = models.CharField(max_length=20, null=False)
+
+
+class Client(User):
+    firstname = models.CharField(max_length=30, null=False)
+    middlename = models.CharField(max_length=30, blank=True)
+    lastname = models.CharField(max_length=30, null=False)
+    phonenumber = models.CharField(max_length=11, null=False)
+    email = models.EmailField()
+    address = models.TextField()
+
+    def __str__(self):
+        return self.ClientID
+
+class Worker(User):
+    WorkerID = models.CharField(max_length=10, primary_key=True, null=False)
+    Status = models.BooleanField()
+
+    def __str__(self):
+        return self.WorkerID
+
+class Service(models.Model):
+    ServiceID = models.CharField(max_length=10, primary_key=True, null=False)
+    WorkerID = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    ServiceType = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ServiceID
--- ServiceYou/RequestService/views.py
+++ ServiceYou/RequestService/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('success')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('success')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration.html', {'form': form})
